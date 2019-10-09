dd3b9545c2643c7c2bfbbde2bef99ce7b9ef9e62
Vi Tiet <tuxedo619_2010@yahoo.com>
2019-10-09 17:51:55 -0400

    Editing Enemy Generator Script

--- Catavaneer-Redo attemp #1/Assets/Scripts/EnemyGenerator.cs
+++ Catavaneer-Redo attemp #1/Assets/Scripts/EnemyGenerator.cs
@@ -1,10 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject[] enemyPrefabs = null;
+    [SerializeField] EnemyPrefab[] enemyPrefabs = null;
     [SerializeField] EnemyTypes generatingType = new EnemyTypes();
-    [SerializeField] float generateInterval = 1f;
+    [SerializeField] float spawnInterval = 1f;
+    [SerializeField] bool random = false;
+
+    private void Update()
+    {
+        // generate
+        // if random generate randomly
+        // if not random generate default
+        Generate(GetPrefabIndexByType(generatingType), random);
+
+    }
+
+    private void Generate(int index, bool isRandom)
+    {
+        if ()
+        Instantiate(enemyPrefabs[index].gameObject);
+    }
+
+    private int GetPrefabIndexByType(EnemyTypes type)
+    {
+        for (int i = 0; i < enemyPrefabs.Length; i++)
+        {
+            if (type == enemyPrefabs[i].type)
+            {
+                return i;
+            }
+        }
+
+        // index out of bound error if can't find type in collecion
+        return -1;
+    }
+
+
+}
+
+[Serializable]
+public class EnemyPrefab
+{
+    public GameObject gameObject;
+    public EnemyTypes type;
 }
